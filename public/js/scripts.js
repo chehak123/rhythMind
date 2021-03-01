e27f824b7e252e0c80c91eadac626a47c7720f6a
chehak123 <chehakagrawal01@gmail.com>
2021-03-01 13:43:59 +0530

    sections added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,44 @@
-//Toggle Button
-function toggleClass()
-{
-	const body=document.querySelector('body');
-	body.classList.toggle('dark');
-	body.style.transition= '0.5s linear';
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
 }
+
+
+jQuery(document).ready(function() {
+	
+	/*
+	    Navigation
+	*/
+	$('a.scroll-link').on('click', function(e) {
+		e.preventDefault();
+		scroll_to($(this), $('nav').outerHeight());
+	});
+	// toggle "navbar-no-bg" class
+	$('.top-content .text').waypoint(function() {
+		$('nav').toggleClass('navbar-no-bg');
+	});
+
+    /*
+        Wow
+    */
+    new WOW().init();
+	
+});
+
+
+jQuery(window).load(function() {
+	
+	/*
+		Hidden images
+	*/
+	$(".testimonial-image img").attr("style", "width: auto !important; height: auto !important;");
+	
+});
